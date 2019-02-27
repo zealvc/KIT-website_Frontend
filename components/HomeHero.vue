9b64047ca58e3cf5877d3c700493a1759961804e
S-Vuthy <sreangvuthy19@gmail.com>
2019-02-27 11:26:17 +0700

    Carousel commit

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -3,32 +3,28 @@
     <v-container
       fluid
       fill-height
-      class="home-hero hidden-sm-and-down"
+      class="home-hero hidden-sm-and-down pa-0"
       style="max-height: 100vh;"
     >
+      <v-carousel hide-controls height="700px">
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
       <v-layout
         justify-start
         row
       >
+
         <v-flex
           md5
           class="offset-md1"
         >
-          <h1 style="padding-top: 70px; padding-bottom: 20px;">
-            Welcome to KIT
-          </h1>
-          <em style="font-size: 25px;">
-            Watch the Season Final Now
-          </em>
-          <p style="padding-top: 20px;">
-            This is the KIT website.KIT is the best school in cambodia.<br>Kirirom Institute of Technology is the best Technology school in CAMBODIA.
-          </p>
         </v-flex>
       </v-layout>
-      <!--<v-spacer/>-->
-      <!--<v-btn style="background-color: red">APPLY NOW</v-btn>-->
     </v-container>
-    <!--For phone-->
     <v-container class="hidden-md-and-up" />
   </span>
 </template>
@@ -36,19 +32,37 @@
 <script>
 export default {
   name: 'HomeHero',
-  components: {}
+  components: {},
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://kit.edu.kh/assets/images/slider/slider1.jpg'
+        },
+        {
+          src: 'https://kit.edu.kh/assets/images/slider/slider3.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
 .home-hero {
-  background: url('https://kit.edu.kh/assets/images/slider/slider1.jpg');
-  background: url('https://kit.edu.kh/assets/images/slider/slider3.jpg');
-  background: url('https://kit.edu.kh/assets/images/slider/slider2.jpg');
+  /*background: url('https://kit.edu.kh/assets/images/slider/slider1.jpg');*/
+  /*background: url('https://kit.edu.kh/assets/images/slider/slider3.jpg');*/
+  /*background: url('https://kit.edu.kh/assets/images/slider/slider2.jpg');*/
   background-size: cover;
   width: 100%;
   height: 100%;
-  margin-top: -70px;
-  padding-top: 70px;
+  margin-top: -170px;
+  padding-top: 75px;
 }
 </style>
